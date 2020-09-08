c4a7874dee7d79efa77be042f826e9d35bed96ff
Ady Drozman <ady.drozman2007@gmail.com>
2020-09-08 17:50:14 +0300

    added filtering,pagination,sorting,selecting on bootcamps

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -8,10 +8,67 @@ const geocoder = require("../utils/geocoder");
 //@access Public
 
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  const bootcamps = await Bootcamp.find();
+  let query;
+  //create copy of the request object
+  const reqQuery = { ...req.query };
+
+  const removeFields = ["select", "sort", "limit", "page"];
+
+  removeFields.forEach((param) => delete reqQuery[param]);
+  console.log(reqQuery);
+
+  let queryString = JSON.stringify(reqQuery);
+  //create mongo $gt,$gte,in operators
+  queryString = queryString.replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+  //filtering
+  query = Bootcamp.find(JSON.parse(queryString));
+
+  //select fields
+
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
+  }
+  //sort
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("-createdAt");
+  }
+
+  const page = parseInt(req.query.page, 10) || 1;
+  const limit = parseInt(req.query.limit, 10) || 100;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  const total = await Bootcamp.countDocuments();
+
+  query = query.skip(startIndex).limit(limit);
+
+  const bootcamps = await query;
+
+  const pagination = {};
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+
   res
     .status(200)
-    .json({ success: true, count: bootcamps.length, data: bootcamps });
+    .json({ success: true, count: bootcamps.length,pagination, data: bootcamps });
 });
 
 //@desc Get a single bootcamp from db,based on the bootcamp id
@@ -77,7 +134,6 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 //@desc Get bootcamps within a range
 //@route GET api/v1/bootcamps/radius/:zipcode/:distance
 //@access Public
-
 
 exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
   const { zipcode, distance } = req.params;
